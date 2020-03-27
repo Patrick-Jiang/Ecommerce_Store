027b34509eebe2060cbee1f436e8c1e2cf12c92f
patrick.jiang <patrick.jiang86@gmail.com>
2020-03-27 14:32:19 -0500

    Seed database from Github Json file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_27_175109) do
+ActiveRecord::Schema.define(version: 2020_03_27_191649) do
+
+  create_table "Products", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.float "price"
+    t.string "image"
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,24 +38,13 @@ ActiveRecord::Schema.define(version: 2020_03_27_175109) do
     t.index ["tag_id"], name: "index_product_tags_on_tag_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.string "descrpition"
-    t.float "price"
-    t.string "image"
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "Products", "categories"
   add_foreign_key "product_tags", "products"
   add_foreign_key "product_tags", "tags"
-  add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,105 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'net/http'
+require 'json'
+require 'faker'
+
+categories = %w[Cleansers Masks SkinCare Makeup BodyCare]
+tags = ['Vegan', 'Cruelty Free', 'Organic', 'Chemical Free', 'Gluten Free', 'Non-GMO', 'Oil Free', 'Water Free']
+ProductTag.destroy_all
+Tag.destroy_all
+Product.destroy_all
+Category.destroy_all
+
+tags.each do |n|
+  Tag.create(name: n)
+end
+
+categories.each do |n|
+  Category.create(name: n)
+end
+
+url = 'https://raw.githubusercontent.com/Patrick-Jiang/The_Face_Shop_Web_Scrape/master/Outputs/bodycare.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+body_care = JSON.parse(response)
+body_care.each do |key|
+  product = Product.create(name: key[1]['name'],
+                           description: key[1]['description'],
+                           price: key[1]['price'].to_f,
+                           image: key[1]['img_link'],
+                           category_id: Category.where(name: 'BodyCare').first.id)
+
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+end
+
+url = 'https://raw.githubusercontent.com/Patrick-Jiang/The_Face_Shop_Web_Scrape/master/Outputs/cleansers.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+cleansers = JSON.parse(response)
+cleansers.each do |key|
+  product = Product.create(name: key[1]['name'],
+                           description: key[1]['description'],
+                           price: key[1]['price'].to_f,
+                           image: key[1]['img_link'],
+                           category_id: Category.where(name: 'Cleansers').first.id)
+
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+end
+
+url = 'https://raw.githubusercontent.com/Patrick-Jiang/The_Face_Shop_Web_Scrape/master/Outputs/makeups.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+makeups = JSON.parse(response)
+makeups.each do |key|
+  product = Product.create(name: key[1]['name'],
+                           description: key[1]['description'],
+                           price: key[1]['price'].to_f,
+                           image: key[1]['img_link'],
+                           category_id: Category.where(name: 'Makeup').first.id)
+
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+end
+
+url = 'https://raw.githubusercontent.com/Patrick-Jiang/The_Face_Shop_Web_Scrape/master/Outputs/masks.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+Masks = JSON.parse(response)
+Masks.each do |key|
+  product = Product.create(name: key[1]['name'],
+                           description: key[1]['description'],
+                           price: key[1]['price'].to_f,
+                           image: key[1]['img_link'],
+                           category_id: Category.where(name: 'Masks').first.id)
+
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+end
+
+url = 'https://raw.githubusercontent.com/Patrick-Jiang/The_Face_Shop_Web_Scrape/master/Outputs/skincare.json'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+SkinCares = JSON.parse(response)
+SkinCares.each do |key|
+  product = Product.create(name: key[1]['name'],
+                           description: key[1]['description'],
+                           price: key[1]['price'].to_f,
+                           image: key[1]['img_link'],
+                           category_id: Category.where(name: 'SkinCare').first.id)
+
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+  ProductTag.create(product_id: product.id,
+                    tag_id: Tag.all.sample(1).first.id)
+end
